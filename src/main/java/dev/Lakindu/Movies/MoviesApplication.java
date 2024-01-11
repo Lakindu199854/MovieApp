f9bc78ebd2fe2618b120b0c60ebe30e98e502dec
Lakindu <Lakidnuat@gmial.com>
2024-01-11 12:53:39 +0400

    Configure the mongodb and connect

--- src/main/java/dev/Lakindu/Movies/MoviesApplication.java
+++ src/main/java/dev/Lakindu/Movies/MoviesApplication.java
@@ -2,12 +2,24 @@ package dev.Lakindu.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
-	}
+		public static void main(String[] args) {
+			SpringApplication.run(MoviesApplication.class, args);
+		}
+
+		@GetMapping("/root")
+		public String root(){
+			return "Hello World";
+		} 
+
+	
 
 }
