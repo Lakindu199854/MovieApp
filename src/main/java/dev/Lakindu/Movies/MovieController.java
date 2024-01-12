0267cf8ace29d4b957da5b6e504cc887328412fd
Lakindu <Lakidnuat@gmial.com>
2024-01-12 12:39:57 +0400

    Set the endpoint to get all movies by id

--- src/main/java/dev/Lakindu/Movies/MovieController.java
+++ src/main/java/dev/Lakindu/Movies/MovieController.java
@@ -1,14 +1,19 @@
 package dev.Lakindu.Movies;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -22,6 +27,11 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.getAllMovies(),HttpStatus.OK);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMethodName(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Movie>>(movieService.getMoviById(id),HttpStatus.OK);
+    }
+    
 }
 
  
--- src/main/java/dev/Lakindu/Movies/MovieService.java
+++ src/main/java/dev/Lakindu/Movies/MovieService.java
@@ -1,7 +1,9 @@
 package dev.Lakindu.Movies;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,11 @@ public class MovieService {
    
     public List<Movie> getAllMovies(){
         return movieRepository.findAll();
+    }
+
+    //If there is no movie found then it has to return null
+    //Because of that we use optional
+    public Optional<Movie> getMoviById(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
