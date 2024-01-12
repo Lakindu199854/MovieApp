5dd25a881298f00c596e72f7782cca0805ce9742
Lakindu <Lakidnuat@gmial.com>
2024-01-12 10:10:25 +0400

    Service layer created

--- src/main/java/dev/Lakindu/Movies/MovieController.java
+++ src/main/java/dev/Lakindu/Movies/MovieController.java
@@ -1,5 +1,8 @@
 package dev.Lakindu.Movies;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/movies")
 //Here every request that comes to this api endpoint is handled by this controlle  r
 public class MovieController {
+    @Autowired
+    //Using this autowire it will inject a instance of MovieService into the movieService
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<String>("All Movies",HttpStatus.OK);
+    //ResponseEntity is a class which represents the entire http resonse including the headers,body and the status code
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(),HttpStatus.OK);
     }
 }
 
