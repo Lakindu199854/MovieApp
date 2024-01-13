8f258710b61c387eb8566be152971d86abce2919
Lakindu <Lakidnuat@gmial.com>
2024-01-14 01:09:08 +0400

    Handled CORS/Creating the GET request to get all movies

---
+++ movieapifrontend/src/Services/MovieServices.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const base_url="http://localhost:8080";
+
+export const getAllMovies=async(path)=>{
+    try{
+        const response=await axios.get(base_url+path);
+        return response;
+    }catch(error){
+        
+    }
+}
